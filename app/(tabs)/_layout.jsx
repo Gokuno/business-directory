50707a68812b4f3c817f4cc5b44449178abd523e
Allan Paz <jallanpazg@gmail.com>
2024-10-30 17:50:58 -0700

    tabs layout

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Tabs } from 'expo-router';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { Colors } from './../../constants/Colors';
+
+export default function TabLayout() {
+    return (
+        <Tabs screenOptions={{ headerShown: false, tabBarActiveTintColor: Colors.dark.icon }}>
+            <Tabs.Screen
+                name="home"
+                options={{
+                    title: "Home",
+                    tabBarIcon: ({ color }) => <AntDesign name="home" size={24} color={color} />
+                }}
+            />
+            <Tabs.Screen
+                name="explore"
+                options={{
+                    title: "Explore",
+                    tabBarIcon: ({ color }) => <AntDesign name="search1" size={24} color={color} />
+                }}
+            />
+            <Tabs.Screen
+                name="profile"
+                options={{
+                    title: "Profile",
+                    tabBarIcon: ({ color }) => <AntDesign name="profile" size={24} color={color} />
+                }}
+            />
+        </Tabs>
+    )
+}
